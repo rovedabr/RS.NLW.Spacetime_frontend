c49e54280c1f1ba078acf321f77c4ab2ac09e7ff
rovedabr <rovedabr@gmail.com>
2023-05-16 10:54:11 -0300

    started front-end NLW 12

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/Button'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <>
+      <h1>Teste</h1>
+      <Button title="teste"/>
+      <Button title="teste 2"/>
+      <Button title="teste 3"/>
+    </>
+  )
+}
---
+++ src/components/Button.tsx
@@ -0,0 +1,15 @@
+const styles = {
+  color: "#F00",
+}
+
+interface ButtonProps {
+  title: String
+}
+
+const name = "Ivan"
+
+export function Button(props: ButtonProps) {
+  return (
+    <p style={styles}>{props.title}</p>
+  )
+}
