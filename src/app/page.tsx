89c9fff748373730a02ddc3fbc8b706d0b7d10e0
rovedabr <rovedabr@gmail.com>
2023-05-17 08:40:36 -0300

    Definido background  e stripes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 export default function Home() {
   return (
-    <main className="grid min-h-screen grid-cols-2">
+    <main className="grid min-h-screen grid-cols-2 bg-[url(../assets/stars.svg)] bg-cover">
       {/* Left */}
-      <div className="relative flex flex-col items-start justify-between px-28 py-16 overflow-hidden">
-        <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2  bg-purple-700 opacity-50 rounded-full blur-full" />
-        <div className="absolute bottom-0 right-0 top-0 w-2 bg-stripes"/>
+      <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 px-28 py-16">
+        <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2  rounded-full bg-purple-700 opacity-50 blur-full" />
+        <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
       </div>
 
       {/* Right */}
-      <div className="flex flex-col p-16">
+      <div className="flex flex-col bg-[url(../assets/stars.svg)] bg-cover p-16">
         <div className="flex flex-1 items-center justify-center">
           <p className="w-[360px] text-center leading-relaxed">
             Você ainda não registrou nenhuma lembrança, comece a{' '}
