64296194b8919dfa0e0a7fc5abcbd32c9810f01b
rovedabr <rovedabr@gmail.com>
2023-05-17 08:13:50 -0300

    Configuração das pages e styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,22 @@
-import { Button } from '@/components/Button'
-import Image from 'next/image'
-
 export default function Home() {
   return (
-    <>
-      <h1>Teste</h1>
-      <Button title="teste"/>
-      <Button title="teste 2"/>
-      <Button title="teste 3"/>
-    </>
+    <main className="grid min-h-screen grid-cols-2">
+      {/* Left */}
+      <div className="relative flex flex-col items-start justify-between px-28 py-16 overflow-hidden">
+        <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2  bg-purple-700 opacity-50 rounded-full blur-full" />
+      </div>
+
+      {/* Right */}
+      <div className="flex flex-col p-16">
+        <div className="flex flex-1 items-center justify-center">
+          <p className="w-[360px] text-center leading-relaxed">
+            Você ainda não registrou nenhuma lembrança, comece a{' '}
+            <a href="" className="underline hover:text-gray-50">
+              criar agora!
+            </a>{' '}
+          </p>
+        </div>
+      </div>
+    </main>
   )
 }
