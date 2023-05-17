d7816970c869e94f3c0763d17327055534ff8d30
rovedabr <rovedabr@gmail.com>
2023-05-17 08:19:36 -0300

    Configuração das pages e styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
       {/* Left */}
       <div className="relative flex flex-col items-start justify-between px-28 py-16 overflow-hidden">
         <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2  bg-purple-700 opacity-50 rounded-full blur-full" />
-        <div className="absolute bottom-0 right-0 top-0 w"/>
+        <div className="absolute bottom-0 right-0 top-0 w-2 bg-stripes"/>
       </div>
 
       {/* Right */}
