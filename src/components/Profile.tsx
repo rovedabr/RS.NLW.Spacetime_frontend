6d3899880d068189069757eeb732efd0a2b4053c
rovedabr <rovedabr@gmail.com>
2023-05-19 10:38:26 -0300

    NewPage

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -17,8 +17,8 @@ export function Profile() {
       <p className="max-w-[140px] text-sm leading-snug">
         {name}
         <a
-          href="/api/auth/logout"
           className="block text-red-400 hover:text-red-300"
+          href="/api/auth/logout"
         >
           Quero sair
         </a>
